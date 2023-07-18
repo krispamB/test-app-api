2e126117c78e8fff4db1d00e7fa2ab446293238d
Christopher Pam <chrystopam@gmail.com>
2023-07-18 20:33:53 +0100

    [feat]: mapped out test controller

--- src/test/test.controller.ts
+++ src/test/test.controller.ts
@@ -1,4 +1,10 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('test')
-export class TestController {}
+export class TestController {
+  faceVerify() {}
+
+  codeVerify() {}
+
+  submitTest() {}
+}
